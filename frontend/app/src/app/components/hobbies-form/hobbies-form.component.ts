90f253cec751e1b4dfa787a8b763cdb1fe75858a
Eoczi <koczi.eduard2311@gmail.com>
2023-12-04 20:37:37 +0200

    get data from hobbies route using EndService

--- frontend/app/src/app/components/hobbies-form/hobbies-form.component.ts
+++ frontend/app/src/app/components/hobbies-form/hobbies-form.component.ts
@@ -2,24 +2,36 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hobby } from '../../modules/hobby.module';
 import { HobbyService } from '../../services/hobbies.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { EndService } from '../../services/end.service';
 
 @Component({
   selector: 'app-hobbies-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './hobbies-form.component.html',
   styleUrl: './hobbies-form.component.css',
-  providers: [HobbyService],
+  providers: [HobbyService, HttpClient, Router],
 })
 export class HobbiesFormComponent {
   hobbies: Hobby[] = [];
-  constructor(private service: HobbyService) {}
+  selectedHobbies: Hobby[] = [];
+  constructor(
+    private service: HobbyService,
+    private router: Router,
+    private end: EndService
+  ) {}
 
   ngOnInit() {
     this.service.getHobbies().subscribe((data) => {
-      console.log(data);
       this.hobbies = data;
     });
+  }
+
+  onSubmit() {
+    this.end.setHobbies(this.selectedHobbies);
+    this.router.navigate(['/universities']);
   }
 }
