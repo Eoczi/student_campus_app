7994f5d6bff0aee9efd117d9e9520ef304a0e18d
Eoczi <koczi.eduard2311@gmail.com>
2023-12-05 16:37:25 +0200

    homepage

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HobbiesFormComponent } from './components/hobbies-form/hobbies-form.component';
 import { UniversitiesComponent } from './components/universities/universities.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: '', component: HobbiesFormComponent },
+  { path: 'hobbies', component: HobbiesFormComponent },
   { path: 'universities', component: UniversitiesComponent },
+  { path: '', component: HomepageComponent },
 ];
