59e24fe74bdd6a7e59ab0dacaa3385ebf7617700
Eoczi <106028382+Eoczi@users.noreply.github.com>
2024-01-11 21:01:45 +0200

    fixed /universities route

--- backend/routes/university.js
+++ backend/routes/university.js
@@ -2,10 +2,19 @@ const express = require("express");
 const connection = require("../database");
 const router = express.Router();
 
-//route that reads all universities and shows everything in a console/json
 router.get("/universities", function (req, res) {
+  const selectedHobbies = req.query;
+  console.log(Object.values(selectedHobbies));
+  console.log(req.url);
+  const query =
+    "SELECT * FROM bli5geofbyhmc9dozgct.Interest JOIN bli5geofbyhmc9dozgct.Faculty on bli5geofbyhmc9dozgct.Interest.faculty_id_fk = bli5geofbyhmc9dozgct.Faculty.id WHERE bli5geofbyhmc9dozgct.Interest.hobby IN (?,?,?);";
   connection.query(
-    "SELECT * FROM bli5geofbyhmc9dozgct.Faculty;",
+    query,
+    [
+      Object.values(selectedHobbies)[0],
+      Object.values(selectedHobbies)[1],
+      Object.values(selectedHobbies)[2],
+    ],
     (error, results) => {
       if (error) {
         console.error("Error querying the database:", error);
--- frontend/app/src/app/components/universities/universities.component.ts
+++ frontend/app/src/app/components/universities/universities.component.ts
@@ -1,31 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Hobby } from '../../modules/hobby.module';
 import { Faculty } from '../../modules/faculty.module';
-import { EndService } from '../../services/end.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UniversitiesService } from '../../services/universities.service';
 
 @Component({
   selector: 'app-universities',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
   templateUrl: './universities.component.html',
   styleUrl: './universities.component.css',
-  providers: [UniversitiesService, HttpClient],
+  providers: [UniversitiesService],
 })
 export class UniversitiesComponent implements OnInit {
-  selectedHobbies: Hobby[] = [];
   faculties: Faculty[] = [];
 
-  //in constructor private end: EndService
   constructor(private service: UniversitiesService) {}
 
   ngOnInit() {
     this.service.getUniversities().subscribe((data) => {
       this.faculties = data;
     });
-    /*this.selectedHobbies = this.end.getHobbies();
-    console.log(this.selectedHobbies);*/
   }
 }
